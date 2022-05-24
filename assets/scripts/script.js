000cafb9d477ce4373fe296e91ed632e51a9efde
TPerschon <timperschon@gmail.com>
2022-05-24 15:27:23 -0600

    Optimized some code, fixed a bug that wasn't yet present

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
+// key for openweather, url for a github an open-source MIT resource I found that has all the openweather cities in a json
 var key = "1f2cac8879443e031fb1b41ac0aa9314";
 var citiesUrl = "https://raw.githubusercontent.com/manifestinteractive/openweathermap-cities/master/data/owm_city_list.json";
 
+// DOM element assignments
 var forecast = $("#forecast");
 var cityspan = $("#cityname");
 var weathericon = $("#weathericon");
@@ -16,6 +18,7 @@ var units = {
     degrees: "F"
 };
 
+// with a given city name, fetch the list of cities and feed that data and the cityname to checkCity()
 function checkWeather(cityname) {
     fetch(citiesUrl)
         .then(function (response) {
@@ -26,20 +29,26 @@ function checkWeather(cityname) {
         });
 };
 
+// if user gave matching city name, get the citie's weather or give an error
 function checkCity(data, cityname) {
+    // loop through all the cities in the list and check the name
     for (i = 0; i < data.RECORDS.length; i++) {
+        // if matching name found
         if (cityname.toUpperCase() === data.RECORDS[i].owm_city_name.toUpperCase()) {
+            // set text of the city span to the city's name
             cityspan.text(data.RECORDS[i].owm_city_name);
-            getCityWeather(data, true);
+            // run getCityWeather with the data
+            getCityWeather(data.RECORDS[i], true);
             return;
         }
     }
     getCityWeather(data, false);
 };
 
+// 
 function getCityWeather(city, boolean = false) {
     if (boolean) {
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.RECORDS[i].owm_latitude}&lon=${city.RECORDS[i].owm_longitude}&appid=${key}&units=${units.system}`)
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.owm_latitude}&lon=${city.owm_longitude}&appid=${key}&units=${units.system}`)
             .then(function (response) {
                 return response.json();
             })
@@ -48,9 +57,30 @@ function getCityWeather(city, boolean = false) {
                 temp.text(`${data.current.temp} \u00B0${units.degrees}`);
                 wind.text(`${data.current.wind_speed} ${units.speed}`);
                 humidity.text(`${data.current.humidity}%`);
-                uv.text(data.current.uvi);
-                colorUv(data.current.uvi, uv);
+                colorUv(data.current.uvi.toFixed(2), uv);
                 weathericon.attr("src", "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
+                for (i = 1; i < 6; i++) { // starting at 1 because 0 is today
+                    // create a div to put each daily forecast in
+                    var forecast = $("<div>").attr("class", "bg-primary text-light rounded m-1 p-1 col-2");
+                    // create and set the text of a h3 with the date in it
+                    var fdate = $("<h3>").text(new Date(data.daily[i].dt * 1000).toLocaleDateString("en-US"));
+                    // create and set the src of an image regarding that day's conditions
+                    var fimg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+                    // create and set the text of a paragraph with the temperature for that day
+                    var ftemp = $("<p>").text(`Temp: ${data.daily[i].temp.day} \u00B0${units.degrees}`);
+                    // create and set the text of a paragraph with the wind for that day
+                    var fwind = $("<p>").text(`Wind: ${data.daily[i].wind_speed} ${units.speed}`);
+                    // create and set the text of a paragraph with the humidity for that day
+                    var fhumidity = $("<p>").text(`Humidity: ${data.daily[i].humidity}%`);
+                    // create and set the text of a span with title for uvi
+                    var fuv = $("<span>").text("UV Index: ");
+                    // create the uvi span
+                    var findex = $("<span>");
+                    // color and format the uvi span
+                    colorUv(data.daily[i].uvi.toFixed(2), findex);
+                    forecast.append(fdate, fimg, ftemp, fwind, fhumidity, fuv, findex)
+                    $("#fivedayforecast").append(forecast)
+                }
             })
     }
     else showError();
@@ -63,11 +93,15 @@ function showError() {
     $("body").append(warning);
 }
 
+// takes in an element and a number
 function colorUv(uvindex, element) {
-    if(uvindex < 3) element.attr("style", "background-color: green;");
-    else if (uvindex < 6) element.attr("style", "background-color: yellow;");
-    else if (uvindex < 8) element.attr("style", "background-color: orange;");
-    else element.attr("style", "background-color: red;");
+    // adds the uv class to the element
+    element.text(uvindex).attr("class", "uv");
+    // if the index is within particular ranges, color the text and background appropriately
+    if(uvindex < 3) element.attr("style", "background-color: green;").addClass("text-light");
+    else if (uvindex < 6) element.attr("style", "background-color: yellow;").addClass("text-dark");
+    else if (uvindex < 8) element.attr("style", "background-color: orange;").addClass("text-dark");
+    else element.attr("style", "background-color: red;").addClass("text-light");
 };
 
 
@@ -76,8 +110,8 @@ function createHistoryItem(city) {
     var item = $("<button>");
     // give the button text
     item.text(city);
-    // give it a timestamp
-    
+    // give it a timestamp this should be done in another place
+    // item.attr("created", Date.now());
     // style the button
     item.attr("class", "col-12 my-1").attr("style", "border-radius: 5px;");
     // append the button to the history section
@@ -92,7 +126,3 @@ $("form").submit(function(event) {
     event.preventDefault();
     checkWeather($("#searchcity").val());
 })
-checkWeather("Los Angeles");
-
-createHistoryItem("Midvale");
-console.log(Date.now())
