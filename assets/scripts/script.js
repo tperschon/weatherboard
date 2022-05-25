b0ae1b41b3859e63d8dab838665e8f7048e8eb17
TPerschon <timperschon@gmail.com>
2022-05-25 09:41:36 -0600

    Modularity in object array functions, formatting on history buttons, more config modularity

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,9 @@ var key = "1f2cac8879443e031fb1b41ac0aa9314";
 var citiesUrl = "https://raw.githubusercontent.com/manifestinteractive/openweathermap-cities/master/data/owm_city_list.json";
 
 // configuration variables
-var maxHistory = 10;
+var maxHistory = 10;    // max number of history items to show
+var daysForecast = 5;   // number of days, up to 7, to get the future weather forecast
+
 // DOM element assignments
 // main forecast elements
 var forecast = $("#forecast");
@@ -16,8 +18,6 @@ var uv = $("#uv");
 // fivedayforecast div
 var fiveday = $("#fivedayforecast");
 
-var test = [];
-
 // local storage retrieval, if (weather)history exists then set it
 var storedWhistory = JSON.parse(localStorage.getItem("whistory"));
 var whistory = [];
@@ -27,29 +27,26 @@ if (storedWhistory) whistory = storedWhistory;
 var units = {
     system: "imperial",
     speed: "MPH",
-    degrees: "F"
+    degrees: "F",
+    language: "en-US"
 };
 
-// with a given city name, fetch the list of cities and feed that data and the cityname to checkCity()
-function checkWeather(cityname) {
+// math to find out width each daily forecast card can take up
+daysForecast = maxNum(daysForecast, 7);
+var forecastWidth = (100 - (daysForecast * 2)) / daysForecast;
+
+// with a given city name, fetch the list of cities and feed that data and the cityname to checkCity(), if something goes wrong show an error
+function getCities(cityname) {
     fetch(citiesUrl)
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            for (a = 0; a < data.length; a++) {
-                var tO = {
-                    city: data[a].owm_city_name,
-                    lat: data[a].owm_latitude,
-                    lon: data[a].owm_longitutde
-                }
-                test.append(tO)
-            }
-            console.log(test)
-            console.log(data)
-            checkCity(data, cityname)
+        .catch(function () {
+            fetchError(citiesUrl)
         })
-        .catch(citiesUrl)
+        .then(function (data) {
+            checkCity(data, cityname);
+        });
 };
 
 // if user gave matching city name, get the citie's weather or give an error
@@ -58,59 +55,58 @@ function checkCity(data, cityname) {
     for (n = 0; n < data.RECORDS.length; n++) {
         // if matching name found
         if (cityname.toUpperCase() === data.RECORDS[n].owm_city_name.toUpperCase()) {
-            //
-            // set text of the city span to the city's name
-            cityspan.text(data.RECORDS[n].owm_city_name);
+            // set text of the city span to the city's name and the current date
+            cityspan.text(`${data.RECORDS[n].owm_city_name} (${new Date(Date.now()).toLocaleDateString(units.language)})`);
             // create a history item of the city
             createHistoryItem(data.RECORDS[n].owm_city_name);
             // (re)populate the history
             populateHistory();
-            console.log(n)
             // run getCityWeather with the data
             getCityWeather(data.RECORDS[n]);
             // return if the city is found so we don't do the error call
             return;
-        }
-    }
+        };
+    };
     // if we get through the loop without a match, given an error
-    searchError();
+    searchError(cityname);
 };
 
-// use given object to make an openweather one call and feed that data to various functions
+// use given object to make an openweather one call and feed that data to various functions, if something goes wrong show an error
 function getCityWeather(city) {
-    console.log(city)
-    console.log(city.owm_latitude)
-    console.log(city.owm_longitude)
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.owm_latitude}&lon=${city.owm_longitude}&appid=${key}&units=${units.system}`)
+    var cityUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.owm_latitude}&lon=${city.owm_longitude}&appid=${key}&units=${units.system}`;
+    fetch(cityUrl)
         .then(function (response) {
             return response.json();
+        })
+        .catch(function () {
+            fetchError(cityUrl)
         })
         .then(function (data) {
             showWeather(data);
             createFiveDayForecast(data);
-        })
+        });
 };
 
 // set up today's weather
 function showWeather(data) {
-    // 
+    // sets all the texts of the DOM elements for the current day's conditions
     temp.text(`${data.current.temp} \u00B0${units.degrees}`);
     wind.text(`${data.current.wind_speed} ${units.speed}`);
     humidity.text(`${data.current.humidity}%`);
     colorUv(data.current.uvi.toFixed(2), uv);
     weathericon.attr("src", "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
-}
+};
 
 // creates the 5-day forecast with the given data object
 function createFiveDayForecast(data) {
     // delete any existing 5-day forecast
     fiveday.children().remove();
     // loop through starting at 1 because 0 is today, create a forecast for each day
-    for (i = 1; i < 6; i++) { 
+    for (i = 1; i < 1 + daysForecast; i++) {
         // create a div to put each daily forecast in
-        var forecast = $("<div>").attr("class", "bg-primary text-light rounded m-1 p-1 col-2");
+        var forecast = $("<div>").attr("class", "bg-primary text-light rounded m-1 p-1").attr("style", `width: ${forecastWidth}%;`);
         // create and set the text of a h3 with the date in it
-        var fdate = $("<h3>").text(new Date(data.daily[i].dt * 1000).toLocaleDateString("en-US"));
+        var fdate = $("<h3>").text(new Date(data.daily[i].dt * 1000).toLocaleDateString(units.language));
         // create and set the src of an image regarding that day's conditions
         var fimg = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
         // create and set the text of a paragraph with the temperature for that day
@@ -137,7 +133,7 @@ function colorUv(uvindex, element) {
     // adds the uv class to the element
     element.text(uvindex).attr("class", "uv");
     // if the index is within particular ranges, color the text and background appropriately
-    if(uvindex < 3) element.attr("style", "background-color: green;").addClass("text-light");
+    if (uvindex < 3) element.attr("style", "background-color: green;").addClass("text-light");
     else if (uvindex < 6) element.attr("style", "background-color: yellow;").addClass("text-dark");
     else if (uvindex < 8) element.attr("style", "background-color: orange;").addClass("text-dark");
     else element.attr("style", "background-color: red;").addClass("text-light");
@@ -153,19 +149,20 @@ function createHistoryItem(city) {
     // push the object to the array
     whistory.push(tempObj);
     // remove duplicate entries from whistory
-    whistory = removeDuplicates(whistory);
+    whistory = removeDuplicates(whistory, "city");
     // set the new whistory to localstorage
     localStorage.setItem("whistory", JSON.stringify(whistory));
 };
 
-// sorts the whistory items based on their timestamp of when they were created, reused code from prior assignment, more recently created items show first
-function sortByTime(objectArray) {
-    objectArray.sort(function (time1, time2) {
-        if (time1.created < time2.created) return -1;
-        else if (time2.created < time1.created) return 1;
+// sorts the whistory items based on a property, reused code from prior assignment
+function sortByProp(objectArray, property, descending) {
+    objectArray.sort(function (prop1, prop2) {
+        if (prop1[property] < prop2[property]) return -1;
+        else if (prop2[property] < prop1[property]) return 1;
         else return 0;
     });
-    objectArray.reverse();
+    // if descending is passed, reverse the array to give descending order
+    if (descending) objectArray.reverse();
 };
 
 function createHistoryButton(city) {
@@ -174,7 +171,7 @@ function createHistoryButton(city) {
     // give the button text
     item.text(city);
     // style the button
-    item.attr("class", "col-12 my-1").attr("style", "border-radius: 5px;");
+    item.attr("class", "col-12 my-1 historybutton").attr("style", "border-radius: 5px;");
     // append the button to the history section
     $("#history").append(item);
 };
@@ -183,57 +180,74 @@ function createHistoryButton(city) {
 function populateHistory() {
     // first remove any history that exists
     $("#history").children().remove();
-    // sort the history
-    sortByTime(whistory);
+    // sort the history by the created property
+    sortByProp(whistory, "created", "literallyanytruthyvalue");
     // create buttons for the history, with a max lnumber of history items
     for (i = 0; i < maxNum(whistory.length, maxHistory); i++) {
         createHistoryButton(whistory[i].city);
     };
 };
 
-// remove duplicates based on the city property from an object array
+// remove duplicates based on given property from an object array
 function removeDuplicates(objectArray, property) {
+    // declare an empty array so we can use array methods
     var newArray = [];
+    // iterate the length of the object array
     for (i = 0; i < objectArray.length; i++) {
+        // start with a false value
         var isDuplicate = false;
+        // iterate the remaining length of the object array (so we don't compare something to itself or past items)
         for (j = i + 1; j < objectArray.length; j++) {
-            if(objectArray[i].city.toUpperCase() === objectArray[j].city.toUpperCase()) {
+            // compare object properties, if true set our boolean true
+            if ((objectArray[i][property]).toUpperCase() === objectArray[j][property].toUpperCase()) {
                 isDuplicate = true;
-            }
-        }
-        if (isDuplicate === false) newArray.push(objectArray[i]);
-    }
+            };
+        };
+        // if the boolean stayed false, push the object to the new array
+        if (!isDuplicate) newArray.push(objectArray[i]);
+    };
+    // return the new array after all the work is done
     return newArray;
-}
+};
 
 // returns the lower of the number of specified maximum
 function maxNum(num, max) {
     if (num > max) return max;
     else return num;
-}
+};
 
-$("#history").click(function(event) {
-    if(event.target.nodeName === "BUTTON") checkWeather(event.target.textContent);
+$("#history").click(function (event) {
+    // clear the search field
+    $("#searchcity").val("");
+    // if a button specifically is clicked within the #history div, pass the button's text into getCities()
+    if (event.target.nodeName === "BUTTON") getCities(event.target.textContent);
 });
 
-$("form").submit(function(event) {
+$("form").submit(function (event) {
+    // stop default submit functionality
     event.preventDefault();
-    checkWeather($("#searchcity").val());
-})
-
+    // pass the search's value into getCities()
+    if ($("#searchcity").val()) getCities($("#searchcity").val());
+    else searchError();
+    // clear the search field
+    $("#searchcity").val("");
+});
 
 // show an error on the search isn't found
-function searchError() {
+function searchError(cityname) {
     // remove any existing warning so they don't stack up
     $("#warning").remove();
-    // create the warning
-    var warning = $("<h4>").text("City could not be found.").attr("class", "text-danger").attr("id", "warning");
-    // append the warning after the search input
-    $("#searchcity").after(warning);
-}
+    // if a value was passed
+    if (cityname) {
+        // create the warning
+        var warning = $("<h6>").text(`'${cityname}' could not be found.`).attr("class", "text-danger").attr("id", "warning");
+        // append the warning after the search input
+        $("#searchcity").after(warning);
+    };
+};
 
 function fetchError(url) {
-    alert(`There was an error retrieving data from the server @ ${url}`)
-}
+    alert(`There was an error retrieving data from the server @ ${url}`);
+};
 
 populateHistory();
